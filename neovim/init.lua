48d0de1584a77c5112fcb062f356cee58ea40255
FynnKw <Fynn.Kwast@btc-ag.com>
2024-06-10 12:58:53 +0000

    added git plugin

--- neovim/init.lua
+++ neovim/init.lua
@@ -245,18 +245,6 @@ require('lazy').setup({
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
