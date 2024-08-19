9b08e1c0992509abbbade6e7a81486ee4aab424e
FynnKw <Fynn.Kwast@btc-ag.com>
2024-08-19 11:53:25 +0200

    added git config

--- home.nix
+++ home.nix
@@ -3,6 +3,36 @@
     enable = true;
     userName  = "FynnKw";
     userEmail = "Fynn.Kwast@btc-ag.com";
+    delta.enable = true;
+    delta.options = {
+        line-numbers = true;
+        side-by-side = true;
+        navigate = true;
+    };
+    extraConfig = {
+        push = {
+            default = "current";
+            autoSetupRemote = true;
+        };
+        merge = {
+            conflictstyle = "diff3";
+        };
+        diff = {
+            colorMoved = "default";
+        };
+    };
+    aliases = {
+        s = "status -s";
+        st = "status";
+        ci = "commit";
+        ciam = "commit --amend --no-edit";
+        co = "checkout";
+        d = "diff";
+        ds = "diff --staged";
+        a = "add";
+        aa = "add --all";
+        lg = "log --pretty=oneline --decorate --graph --abbrev-commit -30";
+    };
   };
   home.stateVersion = "24.05";
 }
